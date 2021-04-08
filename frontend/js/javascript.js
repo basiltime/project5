be95d4651655872166992d9f130744698fabac9c
basiltime <krystalcanfield04@gmail.com>
2021-04-08 16:54:09 -0400

    Text elements retrieved from backend and added to the dom, original markup text content (like item name and description) removed. Favicon added.

--- frontend/js/javascript.js
+++ frontend/js/javascript.js
@@ -1,32 +1,74 @@
+const crossTableName = document.getElementsByClassName('name')[0];
+const crossTableDescription = document.getElementsByClassName('description')[0];
+const crossTablePrice = document.getElementsByClassName('price')[0];
+const crossTableImage = document.getElementsByClassName('image')[0];
 
-const demo = document.getElementById('demo');
+const coffeeTableName = document.getElementsByClassName('name')[1];
+const coffeeTableDescription = document.getElementsByClassName('description')[1];
+const coffeeTablePrice = document.getElementsByClassName('price')[1];
+const coffeeTableImage = document.getElementsByClassName('image')[1];
 
-// Access DOM elements
-const reportSection = document.getElementById('weather-report');
+const diningTableName = document.getElementsByClassName('name')[2];
+const diningTableDescription = document.getElementsByClassName('description')[2];
+const diningTablePrice = document.getElementsByClassName('price')[2];
+const diningTableImage = document.getElementsByClassName('image')[2];
 
+const benchName = document.getElementsByClassName('name')[3];
+const benchDescription = document.getElementsByClassName('description')[3];
+const benchPrice = document.getElementsByClassName('price')[3];
+const benchImage = document.getElementsByClassName('image')[3];
 
+const vintageChairName = document.getElementsByClassName('name')[4];
+const vintageChairDescription = document.getElementsByClassName('description')[4];
+const vintageChairPrice = document.getElementsByClassName('price')[4];
+const vintageChairImage = document.getElementsByClassName('image')[4];
 // Prepare api request
 let apiRequest = new XMLHttpRequest();
 apiRequest.onreadystatechange = () => {
   if (apiRequest.readyState === 4) {
     // Creates error message
     if (apiRequest.status == 404) {
-      alert("Error- no items found")
+      alert("We're sorry! The page you're looking for can't be found. There seems to be a problem on our end.")
+    } else if (apiRequest.status == 500) {
+      alert("Hmm, something went wrong. The server could not be reached. Please try again in a few minutes, and we'll work to fix the problem.")
     } else {
       // Parses JSON response objects to text and displays requested information
-      const response = JSON.parse(apiRequest.response);
-      demo.textContent= response;
-    
-        let firstItem = response[0];
-        // write for each on response
-        console.log(firstItem.name);
-        console.log(firstItem.description);
-        console.log(firstItem.imageUrl);
-        console.dir(firstItem);
-    }
+      response = JSON.parse(apiRequest.response);
+      // Stores each item object into a variable      
+      let crossTable = response[0];
+        crossTableName.textContent = `${crossTable.name}`;
+        crossTableDescription.textContent = `${crossTable.description}`;
+        //crossTableImage.innerHTML = ;
+        crossTablePrice.textContent = `${crossTable.price}`;
+      let coffeeTable = response[1];
+        coffeeTableName.textContent = `${coffeeTable.name}`;
+        coffeeTableDescription.textContent = `${coffeeTable.description}`;
+        //crossTableImage.innerHTML = ;
+        coffeeTablePrice.textContent = `${coffeeTable.price}`;
+      let diningTable = response[2];
+        diningTableName.textContent = `${diningTable.name}`;
+        diningTableDescription.textContent = `${diningTable.description}`;
+        //crossTableImage.innerHTML = ;
+        diningTablePrice.textContent = `${diningTable.price}`;
+      let bench = response[3];
+        benchName.textContent = `${bench.name}`;
+        benchDescription.textContent = `${bench.description}`;
+        //crossTableImage.innerHTML = ;
+        benchPrice.textContent = `${bench.price}`;
+      let vintageChair = response[4];
+        vintageChairName.textContent = `${vintageChair.name}`;
+        vintageChairDescription.textContent = `${vintageChair.description}`;
+        //crossTableImage.innerHTML = ;
+        vintageChairPrice.textContent = `${vintageChair.price}`;
+      
+      }
+     
   }
   
 };
+
+
+
 
 
 window.addEventListener('load', () => {
