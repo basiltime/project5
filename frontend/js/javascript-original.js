3a46f1237c5250179eade3be9067d79ff27cb19c
basiltime <krystalcanfield04@gmail.com>
2021-04-14 17:00:13 -0400

    Refactored get request for list view (home) page. Started working on URL query parameter for single view page.

---
+++ frontend/js/javascript-original.js
@@ -0,0 +1,89 @@
+const norbertName = document.getElementsByClassName('name')[0];
+const norbertDescription = document.getElementsByClassName('description')[0];
+const norbertPrice = document.getElementsByClassName('price')[0];
+const norbertImage = document.getElementsByClassName('image')[0];
+
+const arnoldName = document.getElementsByClassName('name')[1];
+const arnoldDescription = document.getElementsByClassName('description')[1];
+const arnoldPrice = document.getElementsByClassName('price')[1];
+const arnoldImage = document.getElementsByClassName('image')[1];
+
+const lennyAndCarlName = document.getElementsByClassName('name')[2];
+const lennyAndCarlDescription = document.getElementsByClassName('description')[2];
+const lennyAndCarlPrice = document.getElementsByClassName('price')[2];
+const lennyAndCarlImage = document.getElementsByClassName('image')[2];
+
+const gustavName = document.getElementsByClassName('name')[3];
+const gustavDescription = document.getElementsByClassName('description')[3];
+const gustavPrice = document.getElementsByClassName('price')[3];
+const gustavImage = document.getElementsByClassName('image')[3];
+
+const garfunkleName = document.getElementsByClassName('name')[4];
+const garfunkleDescription = document.getElementsByClassName('description')[4];
+const garfunklePrice = document.getElementsByClassName('price')[4];
+const garfunkleImage = document.getElementsByClassName('image')[4];
+
+//Prepare api request
+let apiRequest = new XMLHttpRequest();
+apiRequest.onreadystatechange = () => {
+  if (apiRequest.readyState === 4) {
+    //Creates error message
+    if (apiRequest.status == 404) {
+      alert("We're sorry! The page you're looking for can't be found. There seems to be a problem on our end.")
+    } else if (apiRequest.status == 500) {
+      alert("Hmm, something went wrong. The server could not be reached. Please try again in a few minutes, and we'll work to fix the problem.")
+    } else {
+      //Parses JSON response objects to text and displays requested information
+      response = JSON.parse(apiRequest.response);
+
+      //Stores each item object into a variable      
+      let norbert = response[0];
+        norbertName.textContent = norbert.name;
+        norbertDescription.textContent = norbert.description;
+        norbertPrice.textContent = "$" + norbert.price/100;
+        norbertImage.src = norbert.imageUrl;
+        
+
+      let arnold = response[1];
+        arnoldName.textContent = arnold.name;
+        arnoldDescription.textContent = arnold.description;
+        arnoldPrice.textContent = "$" + arnold.price/100;
+        arnoldImage.src = arnold.imageUrl;
+        
+
+      let lennyAndCarl = response[2];
+        lennyAndCarlName.textContent = lennyAndCarl.name;
+        lennyAndCarlDescription.textContent = lennyAndCarl.description;
+        lennyAndCarlPrice.textContent = "$" + lennyAndCarl.price/100;
+        lennyAndCarlImage.src = lennyAndCarl.imageUrl;
+        
+
+      let gustav = response[3];
+        gustavName.textContent = gustav.name;
+        gustavDescription.textContent = gustav.description;
+        gustavPrice.textContent = "$" + gustav.price/100;
+        gustavImage.src = gustav.imageUrl;
+        
+
+      let garfunkle = response[4];
+        garfunkleName.textContent = garfunkle.name;
+        garfunkleDescription.textContent = garfunkle.description;
+        garfunklePrice.textContent = "$" + garfunkle.price/100;
+        garfunkleImage.src = garfunkle.imageUrl;
+
+     
+      }
+  }
+};
+
+window.addEventListener('load', () => {
+  apiRequest.open('GET', 'http://localhost:3000/api/teddies');
+  apiRequest.send();
+})
+;
+ 
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
--- frontend/js/javascript.js
+++ frontend/js/javascript.js
@@ -1,77 +1,46 @@
-const norbertName = document.getElementsByClassName('name')[0];
-const norbertDescription = document.getElementsByClassName('description')[0];
-const norbertPrice = document.getElementsByClassName('price')[0];
-const norbertImage = document.getElementsByClassName('image')[0];
-
-const arnoldName = document.getElementsByClassName('name')[1];
-const arnoldDescription = document.getElementsByClassName('description')[1];
-const arnoldPrice = document.getElementsByClassName('price')[1];
-const arnoldImage = document.getElementsByClassName('image')[1];
-
-const lennyAndCarlName = document.getElementsByClassName('name')[2];
-const lennyAndCarlDescription = document.getElementsByClassName('description')[2];
-const lennyAndCarlPrice = document.getElementsByClassName('price')[2];
-const lennyAndCarlImage = document.getElementsByClassName('image')[2];
-
-const gustavName = document.getElementsByClassName('name')[3];
-const gustavDescription = document.getElementsByClassName('description')[3];
-const gustavPrice = document.getElementsByClassName('price')[3];
-const gustavImage = document.getElementsByClassName('image')[3];
-
-const garfunkleName = document.getElementsByClassName('name')[4];
-const garfunkleDescription = document.getElementsByClassName('description')[4];
-const garfunklePrice = document.getElementsByClassName('price')[4];
-const garfunkleImage = document.getElementsByClassName('image')[4];
 //Prepare api request
 let apiRequest = new XMLHttpRequest();
 apiRequest.onreadystatechange = () => {
   if (apiRequest.readyState === 4) {
     //Creates error message
     if (apiRequest.status == 404) {
-      alert("We're sorry! The page you're looking for can't be found. There seems to be a problem on our end.")
+      alert("We're sorry! The page you're looking for can't be found.")
     } else if (apiRequest.status == 500) {
-      alert("Hmm, something went wrong. The server could not be reached. Please try again in a few minutes, and we'll work to fix the problem.")
+      alert("Server error. We're working quickly to resolve the issue, please try again later.")
     } else {
       //Parses JSON response objects to text and displays requested information
       response = JSON.parse(apiRequest.response);
 
-      //Stores each item object into a variable      
-      let norbert = response[0];
-        norbertName.textContent = norbert.name;
-        norbertDescription.textContent = norbert.description;
-        norbertPrice.textContent = "$" + norbert.price/100;
-        norbertImage.src = norbert.imageUrl;
-        
+    // Stores each item object into a variable      
 
-      let arnold = response[1];
-        arnoldName.textContent = arnold.name;
-        arnoldDescription.textContent = arnold.description;
-        arnoldPrice.textContent = "$" + arnold.price/100;
-        arnoldImage.src = arnold.imageUrl;
-        
+    const norbertName = document.getElementById('norbert-name').textContent=response[0].name;
+    const norbertDescription = document.getElementById('norbert-description').textContent=response[0].description;
+    const norbertPrice = document.getElementById('norbert-price').textContent='$'+response[0].price/100;
+    const norbertImage = document.getElementById('norbert-image').src=response[0].imageUrl;
+    // practice const norbert = response[0]._id;
+    // practice console.log(norbert);
 
-      let lennyAndCarl = response[2];
-        lennyAndCarlName.textContent = lennyAndCarl.name;
-        lennyAndCarlDescription.textContent = lennyAndCarl.description;
-        lennyAndCarlPrice.textContent = "$" + lennyAndCarl.price/100;
-        lennyAndCarlImage.src = lennyAndCarl.imageUrl;
-        
+    const arnoldName = document.getElementById('arnold-name').textContent=response[1].name;
+    const arnoldDescription = document.getElementById('arnold-description').textContent=response[1].description;
+    const arnoldPrice = document.getElementById('arnold-price').textContent='$'+response[1].price/100;
+    const arnoldImage = document.getElementById('arnold-image').src=response[1].imageUrl;
+    
+    const lennyAndCarlName = document.getElementById('lennyandcarl-name').textContent=response[2].name;
+    const lennyAndCarlDescription = document.getElementById('lennyandcarl-description').textContent=response[2].description;
+    const lennyAndCarlPrice = document.getElementById('lennyandcarl-price').textContent='$'+response[2].price/100;
+    const lennyAndCarlImage = document.getElementById('lennyandcarl-image').src=response[2].imageUrl;
 
-      let gustav = response[3];
-        gustavName.textContent = gustav.name;
-        gustavDescription.textContent = gustav.description;
-        gustavPrice.textContent = "$" + gustav.price/100;
-        gustavImage.src = gustav.imageUrl;
-        
-
-      let garfunkle = response[4];
-        garfunkleName.textContent = garfunkle.name;
-        garfunkleDescription.textContent = garfunkle.description;
-        garfunklePrice.textContent = "$" + garfunkle.price/100;
-        garfunkleImage.src = garfunkle.imageUrl;
-
-     
-      }
+    const gustavName =document.getElementById('gustav-name').textContent=response[3].name;
+    const gustavDescription = document.getElementById('gustav-description').textContent=response[3].description;
+    const gustavPrice = document.getElementById('gustav-price').textContent='$'+response[3].price/100;
+    const gustavImage = document.getElementById('gustav-image').src=response[3].imageUrl;
+    
+    const garfunkleName = document.getElementById('garfunkle-name').textContent=response[4].name;
+    const garfunkleDescription = document.getElementById('garfunkle-description').textContent=response[4].description;
+    const garfunklePrice = document.getElementById('garfunkle-price').textContent='$'+response[4].price/100;
+    const garfunkleImage = document.getElementById('garfunkle-image').src=response[4].imageUrl;
+    
+    }
   }
 };
 
@@ -86,3 +55,9 @@ window.addEventListener('load', () => {
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+const norbertQueryString = '_id=5be9c8541c9d440000665243';
+const myUrlSearchParams = new URLSearchParams(norbertQueryString);
+
+
+
+console.log(myUrlSearchParams);
