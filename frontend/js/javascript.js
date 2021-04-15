dc1f0c1245956c1fcf6531ccbc6a67c0d925f3fb
basiltime <krystalcanfield04@gmail.com>
2021-04-15 00:03:10 -0400

    Refactored code for list view page into a forEach loop.

--- frontend/js/javascript.js
+++ frontend/js/javascript.js
@@ -10,36 +10,20 @@ apiRequest.onreadystatechange = () => {
     } else {
       //Parses JSON response objects to text and displays requested information
       response = JSON.parse(apiRequest.response);
-
-    // Stores each item object into a variable      
-
-    const norbertName = document.getElementById('norbert-name').textContent=response[0].name;
-    const norbertDescription = document.getElementById('norbert-description').textContent=response[0].description;
-    const norbertPrice = document.getElementById('norbert-price').textContent='$'+response[0].price/100;
-    const norbertImage = document.getElementById('norbert-image').src=response[0].imageUrl;
-    // practice const norbert = response[0]._id;
-    // practice console.log(norbert);
-
-    const arnoldName = document.getElementById('arnold-name').textContent=response[1].name;
-    const arnoldDescription = document.getElementById('arnold-description').textContent=response[1].description;
-    const arnoldPrice = document.getElementById('arnold-price').textContent='$'+response[1].price/100;
-    const arnoldImage = document.getElementById('arnold-image').src=response[1].imageUrl;
     
-    const lennyAndCarlName = document.getElementById('lennyandcarl-name').textContent=response[2].name;
-    const lennyAndCarlDescription = document.getElementById('lennyandcarl-description').textContent=response[2].description;
-    const lennyAndCarlPrice = document.getElementById('lennyandcarl-price').textContent='$'+response[2].price/100;
-    const lennyAndCarlImage = document.getElementById('lennyandcarl-image').src=response[2].imageUrl;
 
-    const gustavName =document.getElementById('gustav-name').textContent=response[3].name;
-    const gustavDescription = document.getElementById('gustav-description').textContent=response[3].description;
-    const gustavPrice = document.getElementById('gustav-price').textContent='$'+response[3].price/100;
-    const gustavImage = document.getElementById('gustav-image').src=response[3].imageUrl;
-    
-    const garfunkleName = document.getElementById('garfunkle-name').textContent=response[4].name;
-    const garfunkleDescription = document.getElementById('garfunkle-description').textContent=response[4].description;
-    const garfunklePrice = document.getElementById('garfunkle-price').textContent='$'+response[4].price/100;
-    const garfunkleImage = document.getElementById('garfunkle-image').src=response[4].imageUrl;
-    
+        names = document.getElementsByClassName('name');
+        descriptions = document.getElementsByClassName('description');
+        prices = document.getElementsByClassName('price');
+        images= document.getElementsByClassName('image');
+        
+        response.forEach((item, index) => {
+            names[index].textContent = item.name;
+            descriptions[index].textContent = item.description;
+            prices[index].textContent = item.price;
+            images[index].src = item.imageUrl;
+        });
+
     }
   }
 };
@@ -55,9 +39,13 @@ window.addEventListener('load', () => {
 //////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-const norbertQueryString = '_id=5be9c8541c9d440000665243';
-const myUrlSearchParams = new URLSearchParams(norbertQueryString);
 
 
 
-console.log(myUrlSearchParams);
+
+
+
+
+
+
+
