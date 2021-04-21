0e75a2b026eb57e8df6634548d7d2d5f739190af
basiltime <krystalcanfield04@gmail.com>
2021-04-21 16:33:30 -0400

    Further shortened code for list view page api request by removing unneeded variables

--- frontend/js/javascript.js
+++ frontend/js/javascript.js
@@ -1,29 +1,24 @@
+
+
 //Prepare api request
 let apiRequest = new XMLHttpRequest();
 apiRequest.onreadystatechange = () => {
-  if (apiRequest.readyState === 4) {
-    //Creates error message
-    if (apiRequest.status == 404) {
+  if (apiRequest.readyState === 4) { // If API finishes loading stages
+    
+    if (apiRequest.status == 404) { //Creates error message
       alert("We're sorry! The page you're looking for can't be found.")
     } else if (apiRequest.status == 500) {
       alert("Server error. We're working quickly to resolve the issue, please try again later.")
-    } else {
-      //Parses JSON response objects to text and displays requested information
+    } else { //Parses JSON response objects to text and displays requested information
+      
       response = JSON.parse(apiRequest.response);
-    
-
-        names = document.getElementsByClassName('name');
-        descriptions = document.getElementsByClassName('description');
-        prices = document.getElementsByClassName('price');
-        images= document.getElementsByClassName('image');
-        
+  
         response.forEach((item, index) => {
-            names[index].textContent = item.name;
-            descriptions[index].textContent = item.description;
-            prices[index].textContent = item.price;
-            images[index].src = item.imageUrl;
+            document.getElementsByClassName('name')[index].textContent = item.name;
+            document.getElementsByClassName('description')[index].textContent = item.description;
+            document.getElementsByClassName('price')[index].textContent = `$${item.price/100}`;
+            document.getElementsByClassName('image')[index].src = item.imageUrl;
         });
-
     }
   }
 };
@@ -33,16 +28,57 @@ window.addEventListener('load', () => {
   apiRequest.send();
 })
 ;
- 
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+
+// let apiRequest2 = new XMLHttpRequest();
+// apiRequest2.onreadystatechange = () => {
+//   if (apiRequest2.readyState === 4) {
+//     //Creates error message
+//     if (apiRequest2.status == 404) {
+//       alert("We're sorry! The page you're looking for can't be found.")
+//     } else if (apiRequest2.status == 500) {
+//       alert("Server error. We're working quickly to resolve the issue, please try again later.")
+//     } else {
+//       //Parses JSON response objects to text and displays requested information
+//       response = JSON.parse(apiRequest2.response);
+    
+//         // API WILL RETURN ** ONE ** response object based on the url 
+
+
+   
+//             console.log(response);
+       
+//       //WRITE CODE HERE TO POPULATE SINGLE-ITEM.HTML WITH CONTENT 
+            
+
+//     }
+//   }
+// };
+
+
+
+//generate an ID variable when button is clicked!!!
+
+
+
+
+// When button is clicked, the value of ID is appeneded to the end of the URL insise the GET request. 
+// Depending on the ID, different content will be rendered on the page.
+
+// document.getElementByClassName('btn').addEventListener('click', ($event) => {
+//   let id = $event.target._id;
+//   // apiRequest2.open('GET', `http://localhost:3000/api/teddies/${id}`);
+//   // apiRequest2.send();
+//   console.log(id);
+// });
 
 
 
