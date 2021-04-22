4507cd72a7122b9f9124aea2de8d483dc0dec5f7
basiltime <krystalcanfield04@gmail.com>
2021-04-21 22:25:16 -0400

    Added event listeners to buttons that navigate to single product page. Wrote get request for single product page.

--- frontend/js/javascript.js
+++ frontend/js/javascript.js
@@ -1,6 +1,7 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////  LIST VIEW PAGE /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-//Prepare api request
 let apiRequest = new XMLHttpRequest();
 apiRequest.onreadystatechange = () => {
   if (apiRequest.readyState === 4) { // If API finishes loading stages
@@ -12,16 +13,21 @@ apiRequest.onreadystatechange = () => {
     } else { //Parses JSON response objects to text and displays requested information
       
       response = JSON.parse(apiRequest.response);
+        buttons= document.getElementsByClassName('btn');
   
         response.forEach((item, index) => {
             document.getElementsByClassName('name')[index].textContent = item.name;
             document.getElementsByClassName('description')[index].textContent = item.description;
             document.getElementsByClassName('price')[index].textContent = `$${item.price/100}`;
             document.getElementsByClassName('image')[index].src = item.imageUrl;
-        });
+            buttons[index].setAttribute('name', `${item._id}`);
+
+        })
     }
+    
   }
 };
+
 
 window.addEventListener('load', () => {
   apiRequest.open('GET', 'http://localhost:3000/api/teddies');
@@ -31,54 +37,57 @@ window.addEventListener('load', () => {
 
 
 
-
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////  SINGLE PRODUCT PAGE/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
-// let apiRequest2 = new XMLHttpRequest();
-// apiRequest2.onreadystatechange = () => {
-//   if (apiRequest2.readyState === 4) {
-//     //Creates error message
-//     if (apiRequest2.status == 404) {
-//       alert("We're sorry! The page you're looking for can't be found.")
-//     } else if (apiRequest2.status == 500) {
-//       alert("Server error. We're working quickly to resolve the issue, please try again later.")
-//     } else {
-//       //Parses JSON response objects to text and displays requested information
-//       response = JSON.parse(apiRequest2.response);
+let apiRequest2 = new XMLHttpRequest();
+apiRequest2.onreadystatechange = () => {
+  if (apiRequest2.readyState === 4) {
+    //Creates error message
+    if (apiRequest2.status == 404) {
+      alert("We're sorry! The page you're looking for can't be found.")
+    } else if (apiRequest2.status == 500) {
+      alert("Server error. We're working quickly to resolve the issue, please try again later.")
+    } else {
+      //Parses JSON response objects to text and displays requested information
+      response = JSON.parse(apiRequest2.response);
+
+
+      console.log(response.name);
+      
+      // I expected the below code to work, but it doesn't. Figure out how to get my response to actually appear as content in single-item.html.
+  
+      // document.getElementById('name').textContent = 'hello';
     
-//         // API WILL RETURN ** ONE ** response object based on the url 
-
-
-   
-//             console.log(response);
-       
-//       //WRITE CODE HERE TO POPULATE SINGLE-ITEM.HTML WITH CONTENT 
             
 
-//     }
-//   }
-// };
-
-
-
-//generate an ID variable when button is clicked!!!
+    }
+  }
+};
 
 
 
 
-// When button is clicked, the value of ID is appeneded to the end of the URL insise the GET request. 
-// Depending on the ID, different content will be rendered on the page.
 
-// document.getElementByClassName('btn').addEventListener('click', ($event) => {
-//   let id = $event.target._id;
-//   // apiRequest2.open('GET', `http://localhost:3000/api/teddies/${id}`);
-//   // apiRequest2.send();
-//   console.log(id);
-// });
+// A loop for adding an event listener to each button
+
+let buttons= document.getElementsByClassName('btn');
+
+for (let i = 0; i < buttons.length; i++ ) {
+  document.getElementsByClassName('btn')[i].addEventListener('click', ($event) => {
+    apiRequest2.open('GET', `http://localhost:3000/api/teddies/${buttons[i].getAttribute('name')}`);
+    apiRequest2.send();
+ 
+  });
+}
+
+
+
+
+
 
 
 
