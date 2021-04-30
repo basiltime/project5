395833af7bd9bc021c6b73604c5c7b737b69032f
basiltime <krystalcanfield04@gmail.com>
2021-04-30 14:16:35 -0400

    POST request added

--- frontend/js/cart-page.js
+++ frontend/js/cart-page.js
@@ -43,10 +43,12 @@ let myCart = localStorage;
         let cell1= row.insertCell(0);
         let cell2= row.insertCell(1);
         let cell3= row.insertCell(2);
+        let cell4= row.insertCell(3);
 
-        cell1.textContent = values; // quantity
-        cell2.textContent = response.name; 
+        cell1.textContent = response.name; 
+        cell2.textContent = values; // quantity
         cell3.textContent = `$${response.price/100}`;
+        cell4.textContent = `$${response.price/100 * values}`; // quantity
         }
         }
     }
@@ -55,7 +57,8 @@ let myCart = localStorage;
   }
   
 
- 
+
+// Update quantity displayed in cart icon in navbar
 
   function updateCart() {
     let totalQty = 0;
@@ -72,13 +75,49 @@ let myCart = localStorage;
   updateCart();
 
 
+// Form Validation
 
 
+let submitButton = document.getElementById('submit-button');
+let firstName = document.getElementById('first-name');
+let lastName = document.getElementById('last-name');
+let address = document.getElementById('address');
+let city = document.getElementById('city');
+let email = document.getElementById('email');
 
+submitButton.addEventListener('click', ($event) => {
+  $event.preventDefault();
+  let productsArray = [];
+  for (let i = 0; i < localStorage.length; i++) {
+    let item = localStorage.key(i); // item id
+    productsArray.push(item);
+    
+  }
+  let contactData = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    address: address.value,
+    city: city.value,
+    email: email.value,
+  }
 
+let data = {
+  contact: contactData,
+  products: productsArray
+}
+  
+  const xhr = new XMLHttpRequest();
+  
+  xhr.open('POST', 'http://localhost:3000/api/teddies/order');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.responseType = 'json';
+  xhr.send(JSON.stringify(data));
+   
+  xhr.onload = () => {
+    console.log(xhr.response);
+  };
 
-
-
+});
 
 
 
